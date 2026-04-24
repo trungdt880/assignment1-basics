15f138c755b533a2acf2774d89cc7352069bc978
Trung Dao <bomcon123456@gmail.com>
2026-04-23 21:52:20 -0500

    passed test_tokenizer

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -29,6 +29,12 @@ class Tokenizer:
                 self.special_tokens.append(special_token)
         self.special_tokens_set = set(self.special_tokens)
 
+        self.special_split_pattern = (
+            "(" + "|".join(map(re.escape, self.special_tokens)) + ")"
+        )
+        self.special_split_re = re.compile(self.special_split_pattern)
+        self.word_re = re.compile(self.pattern)
+
     @classmethod
     def from_files(
         cls,
@@ -50,8 +56,7 @@ class Tokenizer:
         if not text:
             return []
         if self.special_tokens:
-            pattern = "(" + "|".join(map(re.escape, self.special_tokens)) + ")"
-            samples = re.split(pattern, text)
+            samples = self.special_split_re.split(text)
         else:
             samples = [text]
 
@@ -60,7 +65,7 @@ class Tokenizer:
             if sample in self.special_tokens_set:
                 result.append(self.vocab_inv[sample.encode("utf-8")])
             else:
-                sample = re.finditer(self.pattern, sample)
+                sample = self.word_re.finditer(sample)
                 for word in sample:
                     word = [bytes([x]) for x in word.group().encode("utf-8")]
 
@@ -93,7 +98,46 @@ class Tokenizer:
         return result
 
     def encode_iterable(self, iterable: Iterable[str]) -> Iterable[int]:
-        return iter([0])
+        for text in iterable:
+            if self.special_tokens:
+                samples = self.special_split_re.split(text)
+            else:
+                samples = [text]
+
+            for sample in samples:
+                if sample in self.special_tokens_set:
+                    yield self.vocab_inv[sample.encode("utf-8")]
+                else:
+                    sample = self.word_re.finditer(sample)
+                    for word in sample:
+                        word = [bytes([x]) for x in word.group().encode("utf-8")]
+
+                        while len(word) > 1:
+                            best_idx = -1
+                            best_rank = float("inf")
+                            best_pair = None
+
+                            for pair_idx, pair in enumerate(zip(word, word[1:])):
+                                pair_rank = self.merge_rank.get(pair, -1)
+                                if pair_rank == -1:
+                                    continue
+                                if pair_rank < best_rank:
+                                    best_idx = pair_idx
+                                    best_rank = pair_rank
+                                    best_pair = pair
+
+                            # can't merge anymore
+                            if best_idx == -1:
+                                break
+
+                            # if found -> merge
+                            word[best_idx] = b"".join(best_pair)
+                            word.pop(best_idx + 1)
+                        for subword in word:
+                            id = self.vocab_inv.get(subword, None)
+                            if id is None:
+                                raise ValueError(f"Subword not in vocab: {subword!r}")
+                            yield id
 
     def decode(self, ids: list[int]) -> str:
         if not ids:
